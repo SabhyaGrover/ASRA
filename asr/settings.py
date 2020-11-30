563f72396e25f2b7262cf1016afa36277d6b53c0
Sabhya <sabgrohyaver@gmail.com>
2020-12-01 02:26:59 +0530

    fix

--- asr/settings.py
+++ asr/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = '7s7s2c^cip(*o%!xekw-@9un$ufj^k_@m)msvhuu^(64ta8em2'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # Application definition

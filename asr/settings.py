da344b29b77755f094bce4e62b2e768f5af6b713
Sabhya <sabgrohyaver@gmail.com>
2020-12-01 14:02:40 +0530

    fix

--- asr/settings.py
+++ asr/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 #import psycopg2
 
 #DATABASE_URL = os.environ['DATABASE_URL']
@@ -80,12 +81,13 @@ WSGI_APPLICATION = 'asr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 

3d66242c9e5c315827537f32ed694b5514b84480
Sabhya <sabgrohyaver@gmail.com>
2021-01-08 17:59:04 +0530

    fix

--- asr/settings.py
+++ asr/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
+# DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Application definition
 
